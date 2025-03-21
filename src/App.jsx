c47f2e22484d5354c2827a06bf32d5af43d541d2
Harsh Agarwal <Realharshagarwal@gmail.com>
2025-03-21 21:14:19 +0530

    Set up a Logo Maker Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import Header from './components/Header'
+import SideNav from './components/SideNav'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <div className='w-64 fixed'>
+      <SideNav/>
+    </div>
+    <div className='ml-64'>Body</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,33 @@
+import { Image, PencilRuler, Shield } from 'lucide-react';
+import React from 'react';
+
+function SideNav() {
+    const menuList=[{
+        id:1,
+        name:'Icon',
+        icon:PencilRuler,
+    },
+    {
+        id:2,
+        name:'Background',
+        icon:Image,
+    },
+    {
+        id:3,
+        name:'Upgrade',
+        icon:Shield,
+    },
+    
+]
+  return (
+    <div className='border shadow-sm h-screen'>
+      {
+        menuList.map((menu,index)=>(
+            <h2 className='p-3 text-lg px-7 text-gray-500 my-2 cursor-pointer hover:bg-gray-900 hover:text-white'
+            key={index}>{menu.name}</h2>
+        ))}
+    </div>
+  );
+}
+
+export default SideNav;
